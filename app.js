0b646843b20000c7b9f350d32a32c452746fcd9b
Roshu-netizen <rohaanali687@gmail.com>
2024-08-28 12:19:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,132 @@
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+function speak(text) {
+  const text_speak = new SpeechSynthesisUtterance(text);
+
+  text_speak.rate = 1.5;
+  text_speak.volume = 1;
+  text_speak.pitch = 1;
+
+  window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe() {
+  var day = new Date();
+  var hour = day.getHours();
+
+  if (hour >= 0 && hour < 12) {
+    speak("Good Morning Boss...");
+  } else if (hour >= 12 && hour < 17) {
+    speak("Good Afternoon Master...");
+  } else {
+    speak("Good Evening Sir...");
+  }
+}
+
+window.addEventListener("load", () => {
+  speak("Welcome To Roshaan Bot");
+  wishMe();
+});
+
+const SpeechRecognition =
+  window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event) => {
+  const currentIndex = event.resultIndex;
+  const transcript = event.results[currentIndex][0].transcript;
+  content.textContent = transcript;
+  takeCommand(transcript.toLowerCase());
+};
+
+btn.addEventListener("click", () => {
+  content.textContent = "Listening...";
+  recognition.start();
+});
+
+function takeCommand(message) {
+  if (message.includes("hey") || message.includes("hello")) {
+    speak("Hello Sir, Its Me Roshaan AI?");
+  } else if (message.includes("open google")) {
+    window.open("https://google.com", "_blank");
+    speak("Opening Google...");
+  } else if (message.includes("open twitter")) {
+    window.open("https://twitter.com", "_blank");
+    speak("Opening Google...");
+  } else if (message.includes("open instagram")) {
+    window.open("https://instagram.com", "_blank");
+    speak("Opening Instagram...");
+  } else if (message.includes("open whatsapp")) {
+    window.open("https://whatsapp.com", "_blank");
+    speak("Opening Whatsapp...");
+  } else if (message.includes("open youtube")) {
+    window.open("https://youtube.com", "_blank");
+    speak("Opening Youtube...");
+  } else if (message.includes("open facebook")) {
+    window.open("https://facebook.com", "_blank");
+    speak("Opening Facebook...");
+  } else if (
+    message.includes("what is") ||
+    message.includes("who is") ||
+    message.includes("what are")
+  ) {
+    window.open(
+      `https://www.google.com/search?q=${message.replace(" ", "+")}`,
+      "_blank"
+    );
+    const finalText =
+      "This is what I found on the internet regarding " + message;
+    speak(finalText);
+  } else if (message.includes("From Instagram Open The Id Of ")) {
+    const userId = message.replace("From Instagram Open The Id Of ", "").trim();
+
+    if (userId) {
+      window.open(`https://www.instagram.com/${userId}`, "_blank");
+      const finalText = `Opening the Instagram profile of ${userId}.`;
+      speak(finalText);
+    } else {
+      window.open(
+        `https://www.instagram.com/search?q=${message.replace(" ", "+")}`,
+        "_blank"
+      );
+      const finalText =
+        "This is what I found on the internet regarding " + message;
+      speak(finalText);
+    }
+  } else if (message.includes("wikipedia")) {
+    window.open(
+      `https://en.wikipedia.org/wiki/${message
+        .replace("wikipedia", "")
+        .trim()}`,
+      "_blank"
+    );
+    const finalText = "This is what I found on Wikipedia regarding " + message;
+    speak(finalText);
+  } else if (message.includes("time")) {
+    const time = new Date().toLocaleString(undefined, {
+      hour: "numeric",
+      minute: "numeric",
+    });
+    const finalText = "The current time is " + time;
+    speak(finalText);
+  } else if (message.includes("date")) {
+    const date = new Date().toLocaleString(undefined, {
+      month: "short",
+      day: "numeric",
+    });
+    const finalText = "Today's date is " + date;
+    speak(finalText);
+  } else if (message.includes("calculator")) {
+    window.open("Calculator:///");
+    const finalText = "Opening Calculator";
+    speak(finalText);
+  } else {
+    window.open(
+      `https://www.google.com/search?q=${message.replace(" ", "+")}`,
+      "_blank"
+    );
+    const finalText = "I found some information for " + message + " on Google";
+    speak(finalText);
+  }
+}
